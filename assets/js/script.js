bf5d30641541b45bdd19b3173c1207aad6b08bd1
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-03-01 17:39:21 -0700

    updated porject cards with photos

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(".project-card").hover(
+    function () {
+        $(this).removeClass("blur").css("cursor", "pointer");
+    },
+    function () {
+        $(this).addClass("blur");
+    }
+);
+
+$('.project-card').on('click', function() {
+    openProject();
+})
+
+
